c3ba1a4e850946591743eb0879b5310342b30c31
siro_paso <inat002asb@gmail.com>
2018-07-25 14:56:04 +0900

    素数判定するプログラム

---
+++ program/lang_c/p25t.c
@@ -0,0 +1,24 @@
+//2~100までの自然数のうち素数を表示するプログラム
+//関数を使っていない無念な方法
+#include <stdio.h>
+#include <math.h>
+
+#define n 2
+#define N 100
+
+int main(){
+  int i,flag;
+
+  for(i=n;i<N+1;i++){
+    if(i==2){
+      flag=1;
+    }
+    if(i%2!=0){
+      //ここ書けない
+      if(<sqrt(double(i))){
+	
+      }
+    }
+  }
+    return 0;
+}
